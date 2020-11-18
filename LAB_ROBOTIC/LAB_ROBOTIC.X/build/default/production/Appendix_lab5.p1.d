1bb0fafa5d85ce210996f62450402a8d47946c7b
kingdiaw <kingdiawehsut@gmail.com>
2020-11-18 15:19:08 +0800

    add appendix lab 5

---
+++ LAB_ROBOTIC/LAB_ROBOTIC.X/build/default/production/Appendix_lab5.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/Appendix_lab5.d  \
+ build/default/production/Appendix_lab5.p1:  \
+ Appendix_lab5.c  \
+picuno.h 
--- LAB_ROBOTIC/LAB_ROBOTIC.X/nbproject/Makefile-default.mk
+++ LAB_ROBOTIC/LAB_ROBOTIC.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=picuno.c lab1.c
+SOURCEFILES_QUOTED_IF_SPACED=picuno.c Appendix_lab5.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/picuno.p1 ${OBJECTDIR}/lab1.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/picuno.p1.d ${OBJECTDIR}/lab1.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/picuno.p1 ${OBJECTDIR}/Appendix_lab5.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/picuno.p1.d ${OBJECTDIR}/Appendix_lab5.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/picuno.p1 ${OBJECTDIR}/lab1.p1
+OBJECTFILES=${OBJECTDIR}/picuno.p1 ${OBJECTDIR}/Appendix_lab5.p1
 
 # Source Files
-SOURCEFILES=picuno.c lab1.c
+SOURCEFILES=picuno.c Appendix_lab5.c
 
 
 
@@ -102,13 +102,13 @@ ${OBJECTDIR}/picuno.p1: picuno.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/picuno.d ${OBJECTDIR}/picuno.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/picuno.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/lab1.p1: lab1.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/Appendix_lab5.p1: Appendix_lab5.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/lab1.p1.d 
-	@${RM} ${OBJECTDIR}/lab1.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1  --debugger=none    --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"     -o${OBJECTDIR}/lab1.p1 lab1.c 
-	@-${MV} ${OBJECTDIR}/lab1.d ${OBJECTDIR}/lab1.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/lab1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/Appendix_lab5.p1.d 
+	@${RM} ${OBJECTDIR}/Appendix_lab5.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1  --debugger=none    --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"     -o${OBJECTDIR}/Appendix_lab5.p1 Appendix_lab5.c 
+	@-${MV} ${OBJECTDIR}/Appendix_lab5.d ${OBJECTDIR}/Appendix_lab5.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Appendix_lab5.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/picuno.p1: picuno.c  nbproject/Makefile-${CND_CONF}.mk
@@ -119,13 +119,13 @@ ${OBJECTDIR}/picuno.p1: picuno.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/picuno.d ${OBJECTDIR}/picuno.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/picuno.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/lab1.p1: lab1.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/Appendix_lab5.p1: Appendix_lab5.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/lab1.p1.d 
-	@${RM} ${OBJECTDIR}/lab1.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G    --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"     -o${OBJECTDIR}/lab1.p1 lab1.c 
-	@-${MV} ${OBJECTDIR}/lab1.d ${OBJECTDIR}/lab1.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/lab1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/Appendix_lab5.p1.d 
+	@${RM} ${OBJECTDIR}/Appendix_lab5.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G    --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_default=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"     -o${OBJECTDIR}/Appendix_lab5.p1 Appendix_lab5.c 
+	@-${MV} ${OBJECTDIR}/Appendix_lab5.d ${OBJECTDIR}/Appendix_lab5.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Appendix_lab5.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
